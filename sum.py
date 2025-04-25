d6be49d11892cbb7731aa35a75bf7d2a0d92dc6c
AustinCodeMaster <austinndiangui.com>
2025-04-25 11:59:49 +0300

    Adding elements in a list

---
+++ sum.py
@@ -0,0 +1,5 @@
+# writing a python code to sum all elements in a list like addition of numbers
+def sum_all(*args):
+     return sum(args)
+total= sum_all(10,13,3,21,7)
+print("The total some of the numbers is:",total)
